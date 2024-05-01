ada2c24c65d121546ee57323a9b43a80b58bb300
theeanthony <acontreras.sing@gmail.com>
2024-05-01 16:26:00 -0700

    Add todo.js

---
+++ src/modules/toDo.js
@@ -0,0 +1,38 @@
+
+
+class ToDo {
+
+    constructor(
+        title,
+        description,
+        dueDate,
+        priority,
+        note,
+        completed = false,
+        completionDate = null
+
+    ){
+
+        this.title = tile;
+        this.description = description; 
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.note = note; 
+
+        this.completed = completed ? true : false; 
+
+        if (Object.prototype.toString.call(completionDate) === '[object Date'){
+            this.completionDate = completionDate; 
+        }
+    }
+    toggleCompletion(){
+            
+        if (completed){
+            this.completed = false ; 
+            this.completionDate = null; 
+        }else{
+            this.completed = true ; 
+            this.completionDate = new Date();  
+        }
+    }
+}
